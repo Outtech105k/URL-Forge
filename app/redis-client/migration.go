c823ec1468b2616b2c6ae5204dd8cfd8e36d201e
Outtech105k <techno510tk@gmail.com>
2026-05-15 17:48:25 +0900

    Reformatted - gofmtでフォーマット修正

--- app/redis-client/migration.go
+++ app/redis-client/migration.go
@@ -8,13 +8,13 @@ func (r *RedisAdapter) MigrateAddPublicCtrl(defaultValue bool) error {
 	iter := r.Client.Scan(0, "*", 0).Iterator()
 	for iter.Next() {
 		key := iter.Val()
-		
+
 		// キーがハッシュ型であることを確認
 		t, err := r.Client.Type(key).Result()
 		if err != nil {
 			return fmt.Errorf("getType for key %s: %w", key, err)
 		}
-		
+
 		if t == "hash" {
 			// public_ctrl が存在しない場合のみデフォルト値をセット
 			if err := r.Client.HSetNX(key, "public_ctrl", defaultValue).Err(); err != nil {
@@ -22,10 +22,10 @@ func (r *RedisAdapter) MigrateAddPublicCtrl(defaultValue bool) error {
 			}
 		}
 	}
-	
+
 	if err := iter.Err(); err != nil {
 		return fmt.Errorf("scan error: %w", err)
 	}
-	
+
 	return nil
 }
