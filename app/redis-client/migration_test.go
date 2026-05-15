817f36d7c77e9b72fd3eafb981f1526bb4b2015d
Outtech105k <techno510tk@gmail.com>
2026-05-15 18:06:23 +0900

    Changed default of `public_ctrl` on migration to false

--- app/redis-client/migration_test.go
+++ app/redis-client/migration_test.go
@@ -8,35 +8,49 @@ import (
 )
 
 func TestMigrateAddPublicCtrl(t *testing.T) {
-	mr, adapter, cleanup := setupTestEnvironment(t)
-	defer cleanup()
+	tests := []struct {
+		name         string
+		defaultValue bool
+	}{
+		{"DefaultTrue", true},
+		{"DefaultFalse", false},
+	}
 
-	// 1. 既存データ（public_ctrl なし）
-	id1 := "mig_id1"
-	mr.HSet(id1, "base_url", "https://example.com/1")
-	mr.HSet(id1, "cushion", "true")
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mr, adapter, cleanup := setupTestEnvironment(t)
+			defer cleanup()
 
-	// 2. 既存データ（既に public_ctrl あり）
-	id2 := "mig_id2"
-	mr.HSet(id2, "base_url", "https://example.com/2")
-	mr.HSet(id2, "public_ctrl", "false")
+			// 1. 既存データ（public_ctrl なし）
+			id1 := "mig_id1"
+			mr.HSet(id1, "base_url", "https://example.com/1")
+			mr.HSet(id1, "cushion", "true")
 
-	// 3. ハッシュ型ではないキー（無視されるべき）
-	id3 := "mig_id3"
-	mr.Set(id3, "just-a-string")
+			// 2. 既存データ（既に public_ctrl あり）
+			// デフォルト値と逆の値をセットして、上書きされないことを確認する
+			id2 := "mig_id2"
+			existingVal := !tt.defaultValue
+			mr.HSet(id2, "base_url", "https://example.com/2")
+			mr.HSet(id2, "public_ctrl", strconv.FormatBool(existingVal))
 
-	// マイグレーション実行
-	err := adapter.MigrateAddPublicCtrl(true)
-	assert.NoError(t, err)
+			// 3. ハッシュ型ではないキー（無視されるべき）
+			id3 := "mig_id3"
+			mr.Set(id3, "just-a-string")
 
-	// 検証: id1 にはデフォルト値 true がセットされていること
-	val1, _ := strconv.ParseBool(mr.HGet(id1, "public_ctrl"))
-	assert.Equal(t, true, val1)
+			// マイグレーション実行
+			err := adapter.MigrateAddPublicCtrl(tt.defaultValue)
+			assert.NoError(t, err)
 
-	// 検証: id2 の既存値 false は上書きされていないこと
-	val2, _ := strconv.ParseBool(mr.HGet(id2, "public_ctrl"))
-	assert.Equal(t, false, val2)
+			// 検証: id1 には指定したデフォルト値がセットされていること
+			val1, _ := strconv.ParseBool(mr.HGet(id1, "public_ctrl"))
+			assert.Equal(t, tt.defaultValue, val1)
 
-	// 検証: id3 はハッシュ型ではないので public_ctrl は存在しないはず
-	assert.Empty(t, mr.HGet(id3, "public_ctrl"))
+			// 検証: id2 の既存値は上書きされていないこと
+			val2, _ := strconv.ParseBool(mr.HGet(id2, "public_ctrl"))
+			assert.Equal(t, existingVal, val2)
+
+			// 検証: id3 はハッシュ型ではないので public_ctrl は存在しないはず
+			assert.Empty(t, mr.HGet(id3, "public_ctrl"))
+		})
+	}
 }
